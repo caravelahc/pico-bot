f828f4ad85417865b4cf988baae308652577ba9f
Diogo Junior <diogo.junior@grad.ufsc.br>
2019-08-12 19:35:50 -0300

    Fix balloon layouts

--- picobot/painter.py
+++ picobot/painter.py
@@ -8,12 +8,13 @@ IMG_DIR = ROOT_DIR + '/images/'
 IMG_NAME = 'img'
 AVATAR_MASK_NAME = 'avatar_mask.png'
 
-AVATAR_SIZE = 36
+AVATAR_SIZE = 42
 MARGIN = 4
 PADDING = 10
-MSG_PADDING_H = 12
-MSG_PADDING_V = 8
-TIME_PADDING = 16
+MSG_PADDING_H = 8
+MSG_PADDING_V = 6
+TIME_PADDING_H = 8
+TIME_PADDING_V = 6
 BOX_HEIGHT = 54
 BOX_MIN_WIDTH = 160
 BOX_MAX_WIDTH = 264
@@ -23,42 +24,41 @@ FONT_SIZE = 14
 TITLE_COLOR = "#338cf3"
 TEXT_COLOR = "#dddddd"
 TIME_COLOR = "#6A7B8C"
-LINE_HEIGHT = 12
+LINE_SPACE = 10
 
 
 def draw_balloon(
-    img_draw: ImageDraw.Draw, xy: list, fill=None, width=0,
-    outline=None, radius=0
+    img_draw: ImageDraw.Draw, xy: list, fill=None, width=0
 ):
     r_x0 = xy[0]
-    r_y0 = xy[1] + radius
+    r_y0 = xy[1] + BOX_RADIUS
     r_x1 = xy[2]
-    r_y1 = xy[3] - radius
+    r_y1 = xy[3] - BOX_RADIUS
     img_draw.rectangle(
-        [r_x0, r_y0, r_x1, r_y1], outline=outline, fill=fill, width=width
+        [r_x0, r_y0, r_x1, r_y1], fill=fill, width=width
     )
-    r_x0 = xy[0] + radius
+    r_x0 = xy[0] + BOX_RADIUS
     r_y0 = xy[1]
-    r_x1 = xy[2] - radius
+    r_x1 = xy[2] - BOX_RADIUS
     r_y1 = xy[3]
     img_draw.rectangle(
-        [r_x0, r_y0, r_x1, r_y1], outline=outline, fill=fill, width=width
+        [r_x0, r_y0, r_x1, r_y1], fill=fill, width=width
     )
-    diam = 2 * radius
+    diam = 2 * BOX_RADIUS
     c_x0 = xy[0]
     c_y0 = xy[1]
     c_x1 = c_x0 + diam
     c_y1 = c_y0 + diam
-    img_draw.ellipse([c_x0, c_y0, c_x1, c_y1], fill=fill, outline=outline, width=width)
+    img_draw.ellipse([c_x0, c_y0, c_x1, c_y1], fill=fill, width=width)
     c_x0 = xy[2] - diam
     c_x1 = c_x0 + diam
-    img_draw.ellipse([c_x0, c_y0, c_x1, c_y1], fill=fill, outline=outline, width=width)
+    img_draw.ellipse([c_x0, c_y0, c_x1, c_y1], fill=fill, width=width)
     c_y0 = xy[3] - diam
     c_y1 = c_y0 + diam
-    img_draw.ellipse([c_x0, c_y0, c_x1, c_y1], fill=fill, outline=outline, width=width)
+    img_draw.ellipse([c_x0, c_y0, c_x1, c_y1], fill=fill, width=width)
 
     arrow_x = 10
-    arrow_y = 10 if radius < 10 else radius
+    arrow_y = 10 if BOX_RADIUS < 10 else BOX_RADIUS
     x0, y0, x1, y1 = xy
     arrow = [
         (x0 - arrow_x, y1),
@@ -88,16 +88,18 @@ def draw_message(
     fill=TEXT_COLOR,
     font=None,
 ):
+    tw = txt_draw.textsize(text, font=font)
     x0 = xy[0] + MSG_PADDING_H
-    y0 = xy[1] + (2 * MSG_PADDING_V) + LINE_HEIGHT
+    y0 = xy[3] - (MSG_PADDING_V + tw[1])
     txt_draw.text((x0, y0), text, font=font, fill=fill)
 
 
 def draw_time(
     txt_draw: ImageDraw.ImageDraw, xy: list, text="04:20", fill=TEXT_COLOR, font=None
 ):
-    x0 = xy[2] - (MSG_PADDING_H * 4)
-    y0 = xy[3] - (MSG_PADDING_V + LINE_HEIGHT)
+    tw = txt_draw.textsize(text, font=font)
+    x0 = xy[2] - TIME_PADDING_H - tw[0]
+    y0 = xy[3] - (TIME_PADDING_V + tw[1])
     txt_draw.text((x0, y0), text, font=font, fill=TIME_COLOR)
 
 
@@ -124,35 +126,52 @@ def draw_avatar(img, avatar_path: str):
 
 
 def sticker_from_text(user_id: int, username: str, text: str, avatar_path: str):
-    size = (512, 2 * PADDING + BOX_HEIGHT)
+    size = (512, 256)
     transparent = (0, 0, 0, 0)
 
     FONT_DIR = ROOT_DIR + '/fonts/'
-    bold_font = ImageFont.truetype(font=f"{FONT_DIR}OpenSans-Bold.ttf", size=13)
-    font = ImageFont.truetype(font=f"{FONT_DIR}OpenSans-Regular.ttf", size=13)
+    bold_font = ImageFont.truetype(font=f"{FONT_DIR}OpenSans-Bold.ttf", size=16)
+    font = ImageFont.truetype(font=f"{FONT_DIR}OpenSans-SemiBold.ttf", size=16)
+    time_font = ImageFont.truetype(font=f"{FONT_DIR}OpenSans-Regular.ttf", size=13)
 
-    aux_text = username if (len(username) >= len(text)) else text
-    aux_text += '88:88'
-    aux_font = bold_font if (len(username) >= len(text)) else font
-    aux = ImageDraw.Draw(Image.new('RGBA', size, transparent))
-    box_width = aux.textsize(aux_text, font=aux_font)[0] + 2 * MSG_PADDING_H + TIME_PADDING
-    box_width = max(BOX_MIN_WIDTH, min(box_width, BOX_MAX_WIDTH))
-    img_width = min(512, 2 * MARGIN + AVATAR_SIZE + PADDING + box_width)
-    size = (img_width, size[1])
+    username = username if (len(username) < 26) else f'{username[0:25]}...'
+    limit_is_user = (len(username) >= len(text))
+    aux_img = ImageDraw.Draw(Image.new('RGBA', size, transparent))
+    if limit_is_user:
+        aux_text = username
+        text_size = aux_img.textsize(aux_text, font=bold_font)
+        box_size = (text_size[0] + 2 * MSG_PADDING_H + TIME_PADDING_H, text_size[1] + 2 * MSG_PADDING_V + 2 * LINE_SPACE)
+        final_text = text
+    else:
+        aux_text = text + '8888'
+        if len(text) < 26:
+            text_size = aux_img.textsize(aux_text, font=font)
+            final_text = text
+        else:
+            aux_text = wrapped_text(text, line_limit=26)
+            text_size = aux_img.multiline_textsize(aux_text, font=font)
+            final_text = aux_text
+        box_size = (text_size[0] + 2 * MSG_PADDING_H + TIME_PADDING_H, text_size[1] + 2 * MSG_PADDING_V + 2 * LINE_SPACE)
+
+    b_width = max(BOX_MIN_WIDTH, box_size[0])
+    b_height = box_size[1]
+    img_width = min(512, 2 * MARGIN + AVATAR_SIZE + PADDING + b_width)
+    size = (img_width, 4 * PADDING + b_height)
 
     img = Image.new("RGBA", size, transparent)
     dr = ImageDraw.Draw(img)
     draw_avatar(img, avatar_path)
 
     x0 = MARGIN + PADDING + AVATAR_SIZE
-    x1 = x0 + box_width
-    y1 = PADDING + BOX_HEIGHT
+    x1 = x0 + b_width
+    y1 = PADDING + b_height
     xy = [x0, PADDING, x1, y1]
-    draw_balloon(dr, xy=xy, fill=BOX_COLOR, radius=BOX_RADIUS)
+    draw_balloon(dr, xy=xy, fill=BOX_COLOR)
 
     draw_username(dr, xy=xy, font=bold_font, username=username)
-    draw_message(dr, xy=xy, font=font, text=text)
-    draw_time(dr, xy=xy, font=font)
+    draw_message(dr, xy=xy, font=font, text=final_text)
+    draw_time(dr, xy=xy, font=time_font)
+    img.save(f'{IMG_DIR}kkroto.png')
 
     ratio = 512 / img_width
     sample = Image.ANTIALIAS
@@ -162,6 +181,17 @@ def sticker_from_text(user_id: int, username: str, text: str, avatar_path: str):
     img.save(img_path)
     img.close()
     return img_path
+
+
+def wrapped_text(text: str, line_limit=25):
+    words = text.split(' ')
+    lines = [words[0]]
+    for w in words[1:]:
+        if len(lines[-1]) + len(w) < line_limit:
+            lines[-1] += ' ' + w
+        else:
+            lines.append(w)
+    return '\n'.join(lines)
 
 
 def sticker_from_image(jpg_path: str):
@@ -183,7 +213,7 @@ def generate_avatar_mask(size: tuple):
     y1 = BOX_HEIGHT + PADDING
     xy = [MARGIN, y0, MARGIN + AVATAR_SIZE, y1]
 
-    img = Image.new("RGBA", size, (0,0,0,0))
+    img = Image.new("RGBA", size, (0, 0, 0, 0))
     maskdraw = ImageDraw.Draw(img)
     maskdraw.ellipse(xy, fill='#FFFFFF')
     del maskdraw
--- picobot/responses.py
+++ picobot/responses.py
@@ -13,29 +13,28 @@ Por favor, primeiro crie um utilizando o comando /newpack para criar um novo pac
 
 ADDED_STICKER = 'Sticker adicionado!'
 
-GREETING = 'Hello!'
+GREETING = """Olá! Bot para automatizar a criação de stickers \
+a partir de imagens ou mensagens.
+Utilize o comando /help para ver os comandos disponíveis.
+
+Código disponível no repositório:
+http://github.com/caravelahc/pico-bot
+
+Feito por @diogojs e @caravelahc"""
 
 ACCESS_DENIED = """Username is not in the sudoers file.
 This incident will be reported."""
 
-HELP_MSG = """Bot para automatizar a criação de stickers \
-    a partir de imagens ou mensagens.
-    Para ativar o bot utilize /start
-    Comandos:
+HELP_MSG = """Comandos:
     /newpack <NomeDoPack> - cria um novo pack/pacote
     /setpackicon - define o ícone do pack
     /addsticker <NomeDoPack> [emoji]:
-        Adiciona sticker a um pack, com o respectivo emoji.
-        Envie esse comando como legenda de uma imagem, \
-        em resposta a uma imagem/mensagem para criar um novo sticker, \
-        ou como resposta a um sticker para apenas adicioná-lo ao pack.
+        Adiciona sticker a um pack, com o respectivo emoji. \
+Envie esse comando como legenda de uma imagem, \
+em resposta a uma imagem/mensagem para criar um novo sticker, \
+ou como resposta a um sticker existente para apenas adicioná-lo ao pack.
     /delsticker - remove o sticker do pack (não recuperável)
 
     Este bot também reflete os demais comandos do bot oficial de \
-    stickers do telegram, para mais informações verifique a ajuda \
-    do @Stickers
-
-    Código disponível no repositório:
-    http://github.com/caravelahc/pico-bot
-
-    Feito por @diogojs"""
+stickers do telegram, para mais informações verifique a ajuda \
+do @Stickers"""
