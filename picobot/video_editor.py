f48a926524968ba54a9a2d17cf99151ec92971ed
Arthur Mesquita Pickcius <arthur.pickcius@protonmail.com>
2022-05-24 21:14:14 -0300

    - create video sticker from another sticker - criate video sticker from circle video

--- picobot/video_editor.py
+++ picobot/video_editor.py
@@ -2,7 +2,11 @@ from pathlib import Path
 import ffmpeg
 
 
-def sticker_from_video(video_path: Path):
+class VideoTooLongError(Exception):
+    pass
+
+
+def sticker_from_video(video_path: Path, circle_mask: Path = None):
     '''
     Converts the given video to a proper format that can be uploaded as a sticker.
     Telegram accepts WEBM VP9 with a maximum size of 512x512 pixels with maximum 3 seconds duration.
@@ -16,16 +20,34 @@ def sticker_from_video(video_path: Path):
     duration = float(probe['format']['duration'])
 
     if duration > 3.0:
-        raise ValueError('Video duration exceeds limits')
+        raise VideoTooLongError('Video duration exceeds limits')
 
     (width, height) = estimate_video_sticker_size(width, height)
     vid_output_path = video_path.with_suffix('.webm')
-    (
-        ffmpeg.input(video_path)
-        .video.filter('scale', width, height)
-        .output(filename=vid_output_path.as_posix(), format='webm', vcodec='libvpx-vp9')
-        .run(overwrite_output=True)
-    )
+    vid_filename = video_path.name
+    in_file = ffmpeg.input(video_path).filter('scale', width, height)
+    if circle_mask is None:
+        (
+            in_file.output(
+                filename=vid_output_path.as_posix(),
+                format='webm',
+                vcodec='libvpx-vp9',
+                pix_fmt='yuva420p',
+            ).run(overwrite_output=True)
+        )
+    else:
+        mask = ffmpeg.input(circle_mask).filter('alphaextract')
+        (
+            ffmpeg.filter((in_file, mask), 'alphamerge',)
+            .output(
+                filename=vid_output_path.as_posix(),
+                format='webm',
+                vcodec='libvpx-vp9',
+                pix_fmt='yuva420p',
+            )
+            .run(overwrite_output=True)
+        )
+
     return vid_output_path
 
 
